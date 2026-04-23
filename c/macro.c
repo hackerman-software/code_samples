d84ce5a13b376864bbdee4912f3aef43a3908866
Michael Sjöberg <mail@michaelsjoeberg.com>
2026-04-23 23:36:42 +0800

    Update macro.c

--- c/macro.c
+++ c/macro.c
@@ -17,16 +17,16 @@
 #define max(x, y) ((x) > (y) ? (x) : (y))
 
 int main() {
-   printf("%s\n", __FILE__);            // macros.c
-   printf("%s\n", __DATE__);            // Aug 21 2019
-   printf("%s\n", __TIME__);            // 01:22:18
-   printf("%d\n", __LINE__);            // 12
-   printf("%d\n", __STDC__);            // 1
+    printf("%s\n", __FILE__); // macros.c
+    printf("%s\n", __DATE__); // Aug 21 2019
+    printf("%s\n", __TIME__); // 01:22:18
+    printf("%d\n", __LINE__); // 12
+    printf("%d\n", __STDC__); // 1
 
-   error_message("This is an error.");  // error: "This is an error."
+    error_message("This is an error."); // error: "This is an error."
 
-   printf("%s\n", MESSAGE);             // This is the message if not already defined.
+    printf("%s\n", MESSAGE); // This is the message if not already defined.
 
-   printf("%d\n", square(2));           // 4
-   printf("%d\n", max(4,5));            // 5
+    printf("%d\n", square(2)); // 4
+    printf("%d\n", max(4,5)); // 5
 }
